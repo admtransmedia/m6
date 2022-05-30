bdfbe1d4e8f42aff36845d6cb01bdb6db0b4bdc9
Sergei Kriukov <admtransmedia@icloud.com>
2022-05-30 17:47:18 +0600

    start

--- CitySightsApp/CitySightsApp.swift
+++ CitySightsApp/CitySightsApp.swift
@@ -1,5 +1,5 @@
 //
-//  CitySightsAppApp.swift
+//  CitySightsApp.swift
 //  CitySightsApp
 //
 //  Created by Sergei Kriukov on 30.05.2022.
@@ -8,10 +8,10 @@
 import SwiftUI
 
 @main
-struct CitySightsAppApp: App {
+struct CitySightsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LaunchView()
         }
     }
 }
