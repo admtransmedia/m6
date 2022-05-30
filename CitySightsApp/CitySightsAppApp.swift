552d1033bbeb1e67fd4fdeba864015ec9812f1a4
Sergei Kriukov <admtransmedia@icloud.com>
2022-05-30 17:14:41 +0600

    Initial Commit

---
+++ CitySightsApp/CitySightsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsAppApp.swift
+//  CitySightsApp
+//
+//  Created by Sergei Kriukov on 30.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
